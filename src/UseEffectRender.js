e420841b52ce17d81e4aa50f58e1b266e0deba02
ohmoto-2020 <vitamin29290209@gmail.com>
2023-06-25 18:25:50 +0900

    add:UseEffectテスト

---
+++ src/UseEffectRender.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+
+const UseEffectRender = () => {
+  const [user, setUser] = useState(null)
+
+  const fetchJSON = async () => {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users/1")
+    return res.data
+  }
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await fetchJSON()
+      setUser(user)
+    }
+    fetchUser()
+  }, [])
+
+  return (
+    <>
+      {user ? (
+        <p>
+          I am {user.username} : {user.email}
+        </p>
+      ) : null}
+    </>
+  )
+}
+
+export default UseEffectRender
